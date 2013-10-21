2e93a8ab0a123145ffc20e68de277856e704c5bf
Justin M Shuck <jushuck@chandra.cs.pdx.edu>
2013-10-20 19:13:24 -0700

    i added the pseduocode text file after I created some test cases in the test log

--- sequ.py
+++ sequ.py
@@ -1,1 +1,10 @@
-##
+# This program is a modified seq command called sequ. This source code will hose all 5 compliance levels.
+# Name: Justin Shuck
+# Copy write  Justin Shuck
+
+
+import sys
+
+if len(sys.argv)
+print 'Number of args:',len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)

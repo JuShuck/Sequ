e9efe50d1ef04b568e967b1c153b30993fa1a69e
Justin M Shuck <jushuck@fab03.cecs.pdx.edu>
2013-11-10 10:15:14 -0800

    Added -s,-f, and -v. Need to test edge cases.

--- sequ.py
+++ sequ.py
@@ -8,41 +8,82 @@ import numbers,argparse
 #Current version of sequ
 __VERSION =  '1.0.0'
 
+
+#will print the sequence
+def printSeq(lower,increment,upper):
+    while lower <= upper:
+        print(lower)
+        lower = lower + increment 
+def printf(format,*args):
+    sys.stdout.write(format % args)
+
+# Define arguments that are handled in this program
 parser = argparse.ArgumentParser()
-parser.add_argument("--version","-v",help="increase",action="store_true")
+parser.add_argument("--version","-v", help="View the current version of sequ", action="store_true")
+parser.add_argument("--separator","-s", help="Seperate the sequence by a string", action="store_true")
+parser.add_argument("--format","-f", help = "Uses Floating-point Format",action = "store_true")
+
+#Postionals
+parser.add_argument("string", nargs="?", default= " ")
+parser.add_argument("lower", type = int, nargs = "?", help = "Lower bounds of the sequence")
+parser.add_argument("upper", type = int, nargs = "?", help = "Upper bounds of the sequence")
+
 args = parser.parse_args()
 if args.version:
     print("version"+__VERSION)
+    exit(1)
+if args.separator:
+    print("Separator mode!"+str(argv[2]))
+    if len(argv) ==4:
+        lower = int(str(argv[2]))
+        upper = int(str(argv[3]))
+        string = "\n"
+    else:
+        lower = int(str(argv[3]))
+        upper = int(str(argv[4]))
+        string = str(argv[2])   
+    while lower < upper:
+        print lower,
+        print string,
+        lower+=1       
+    exit(1)
+if args.format:
+    print("Format Mode")
+    if len(argv) == 4:
+        lower = float(str(argv[2]))
+	upper = float(str(argv[3]))
+        increment = 1.0
+    else:
+        lower = float(str(argv[2]))
+	upper = float(str(argv[4]))
+	increment = float(str(argv[3]))
 
-# Tests to see if the -v or --version flag is 
-if str(argv[1]) == '-v' or str(argv[1]) == '--version':
-    print("sequ version "+__VERSION)
+    while lower <= upper:
+        print lower
+        lower+=increment
     exit(1)
 
 # Used to verify that there is not more than 2 arguments(besides the file)
-if len(argv) < 2 or len(argv) > 5:
+if len(argv) < 2 or len(argv) > 6:
     print('Error: Invalid number of arguments.')
     exit(1)
 
 
 #Checks to verify that the arguments are both integers. will terminate with an error if one of the arguments is bad.
-try:
+if len(argv) >= 3:
+    try:
+	#castInt()
 #cast the arguments to int types
-    a = int(str(argv[1]))
-    b = int(str(argv[2]))
-except ValueError:
-    print("Error: Invalid argument type")
-    exit(1)
-#If the arguments are the same
-if a == b:
-   print(a)
-#If the 2nd argument is smaller than the first it will print in ascending order
-if b > a:
-    for x in range (a,b+1):
-        print(x)
+        a = int(str(argv[1]))
+        c = int(str(argv[2]))
+        if len(argv) == 4:
+            c = int(str(argv[3]))
+            b = int(str(argv[2]))
+        else:
+            b = 1
+    except ValueError:
+        print("Error: Invalid argument type")
+        exit(1)
 
-#If the 2nd argument is larger than the first, it will print in descending order
-if b < a:
-    while b <= a:
-        print(a)
-        a -= 1
+
+printSeq(a,b,c)

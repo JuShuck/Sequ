6fc4735a49e65efccc7eacf85d227fc74f353e71
Justin M Shuck <jushuck@fab03.cecs.pdx.edu>
2013-11-12 20:58:18 -0800

    After attending class today, I found more cases to test and fixed them. Mis-understood the specs and clarified what CL1 requires. Made changes to satisfy CL1

--- sequ.py
+++ sequ.py
@@ -10,17 +10,23 @@ import numbers,argparse,sys
 __VERSION =  '1.0.0'
 
 
+#Default invalid input error and exit
+def invalidInput():
+    print 'Error: Invalid Input.'
+    exit(1)
+
 #will print the sequence that has a lower and upper bound and an increment.
 def printSeq(lower,increment,upper):
     while lower <= upper:
         print(lower)
         lower = lower + increment 
+
 #emulates the printf function that is used in C
 def printf(format,*args):
-    sys.stdout.write(format %args  )
+    sys.stdout.write(format %args )
     print
 
-#Verifys the argument is an int. If the cast fails, it is not an int.
+#Verifys the argument is an int. If the cast fails, it is not an int, it then trys to cast to a float. If it is neither the program terminates
 def verifyArg(argument,x):
     try:
         argument = int(str(argv[x]))
@@ -31,8 +37,19 @@ def verifyArg(argument,x):
 	argument = float(str(argv[x]))
 	return argument
     except ValueError:
-        print "1.Error: Invalid input"
+        invalidInput()
     	exit(1)
+
+#will go through the sequence and find the max width and return it
+def findMaxWidth(lower,inc,upper):
+    maxWidth = 0
+    while lower <= upper:
+	if len(str(lower)) > maxWidth:
+	    maxWidth = len(str(lower))
+        lower = lower + inc
+	if inc == 0:
+	    return maxWidth
+    return maxWidth
 
 # Define arguments that are handled in this program
 parser = argparse.ArgumentParser()
@@ -56,88 +73,120 @@ if args.version == 1:
     print"SEQU Version "+__VERSION
     exit(1)
 
-#-s and --separator
+#-s and --separator, the default string separator is ""
 if args.separator == 1:
-    #If the form is of 'sequ.py -s 1 10' i.e no position after -s flag
-    if len(argv) ==4:
-	#verify if the variables are in the proper position
+    #has nothing after the -s flag
+    if len(argv) == 2:
+        invalidInput()
+    #has one argument after the -s flag
+    if len(argv) == 3:
+        upper = verifyArg(str(argv[2]),2)
+	lower = 1
+        string = ""
+    #has two arguments after the -s flag
+    elif len(argv) == 4:
 	lower = verifyArg(str(argv[2]),2)
 	upper = verifyArg(str(argv[3]),3)
-        string = "\n"		#default string separator
-    #The string is present after the -s flag
+        string = ""		
+    #has three arguments after the -s flag. (string, lower, upper)
     else:
         lower = verifyArg(str(argv[3]),3)
         upper = verifyArg(str(argv[4]),4)
         string = str(argv[2])   
-    #Prints the sequence with the string as a separator
+
+    #Prints the sequence with the string as a separator and then exits
     while lower <= upper:
-        print lower, string,
+        print lower
+	print string
         lower+=1       
-    
     exit(1)
 
-#-f and --format
+#-f and --format, the default format is '%g'
 if args.format == 1:
-    #No positional after the -f flag
-    if len(argv) == 4:
-	#verify if the variables are in the proper position
+    #nothing after the -f flag
+    if len(argv) == 2:
+        invalidInput()
+    #One argument after the -f flag
+    if len(argv) == 3:
+        upper = verifyArg(str(argv[2]),2)
+        lower = 1
+	formatType = "%g"
+    #Two arguments after the -f flag
+    elif len(argv) == 4:
         lower = verifyArg(str(argv[2]),2)
         upper = verifyArg(str(argv[3]),3)
-        formatType = "%g"	#default format type
-
-	#Prints the sequence with the printf function
-        while lower <= upper:
-	    printf(formatType,lower)
-	    lower+=1
-	exit(1)
-
+        formatType = "%g"
+    #has three arguments after the -f flag. (format,lower,upper)
     else:
         lower = verifyArg(str(argv[3]),3)
         upper = verifyArg(str(argv[4]),4)
 	formatType = str(argv[2])
     
-	#Accepted format types that this program accepts
-	accepted = {'%g','%X','%e','%f'}
+    #Accepted format types that this program accepts
+    accepted = {'%g','%X','%e','%f'}
 
-	#Prints if the user entered a proper format type, otherwise exit
-	if formatType in accepted:
-            while lower <= upper:
-                printf(formatType,lower)
-                lower+=1
-	    exit(1)
-        else:
- 	    print 'Error: Invalid format Type.'
-	    exit(1)	
+    #Prints if the user entered a proper format type, otherwise exit
+    if formatType in accepted:
+        while lower <= upper:
+            printf(formatType,lower)
+            lower+=1
+        exit(1)
+    else:
+        print 'Error: Invalid format Type.'
+        exit(1)	
 
-#-w and --equalwidth
+#-w and --equalwidth, default width is 1
 if args.equalwidth == 1:
-    #Verifys if there is an increment variable ( Lower, Increment, Upper)
-    if len(argv) == 4:
+    #Nothing after the -w flag
+    if len(argv) == 2:
+	invalidInput()
+    #One argument after the -w flag
+    if len(argv) == 3:
+         lower = 1
+         upper = verifyArg(str(argv[2]),2)
+         increment = 1
+         maxWidth = findMaxWidth(0,1,verifyArg(str(argv[2]),2))
+    #Two arguments after the -w flag
+    elif len(argv) == 4:
          lower = verifyArg(str(argv[2]),2)
          upper = verifyArg(str(argv[3]),3)
-	 increment = 1			#Default increment is 1
-	 maxWidth = len(str(argv[3]))	#Gets the Max Width from the Upper argument
+	 increment = 1
+	 maxWidth = findMaxWidth(verifyArg(str(argv[2]),2),1,verifyArg(str(argv[3]),3))
+    #Has three arguments after the -w flag. (lower,increment,upper)
     else:
 	 lower = verifyArg(str(argv[2]),2)
 	 upper = verifyArg(str(argv[4]),4)
 	 increment = verifyArg(str(argv[3]),3)
-         maxWidth = len((argv[4]))	#Gets the Max Width from the Upper argument
+         maxWidth = findMaxWidth(verifyArg(str(argv[2]),2),verifyArg(str(argv[3]),3),verifyArg(str(argv[4]),4))
 
     #Prints the sequence with equal width using zfill
     while lower <= upper:
          print str(lower).zfill(maxWidth)
 	 lower = lower+increment
-
     exit(1)
 
 
-# Used to verify that there is not more than 2 arguments(besides the file)
-if len(argv) <= 2 or len(argv) > 6:
+# Used to verify Invalid number of arguments
+if len(argv) <= 1 or len(argv) > 6:
     print('Error: Invalid number of arguments.')
     exit(1)
 
 
-#Checks to verify that the arguments are both integers. will terminate with an error if one of the arguments is bad.
+#This will be used if there are no flags in front of the numbers if no increment is given the default is 1
+if len(argv) == 2:
+    try:
+	int(str(argv[1]))
+        lower = 1
+	increment = 1
+    except:
+	try:
+	    float(str(argv[1]))
+            lower = 1.0
+	    increment = 1.0
+        except:
+            invalidInput()
+    upper = verifyArg(str(argv[1]),1)
+#if there is not
 if len(argv) >= 3:
 
     lower = verifyArg(str(argv[1]),1)
@@ -147,6 +196,5 @@ if len(argv) >= 3:
         upper = verifyArg(str(argv[3]),3)
         increment = verifyArg(str(argv[2]),2)
     else: 
-        increment = 1	#defaults increment at 1
-
-    printSeq(lower,increment,upper)
+        increment = 1
+printSeq(lower,increment,upper)
